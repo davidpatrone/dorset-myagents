54584c41b0234eddcc61f4f6a2a0da3bec5b9ec0
Patrone <patrodm1@patrodm1-ml1.jhuapl.edu>
2016-09-08 14:32:27 -0400

    Initial push of simple agents and dev tools for agent development

---
+++ src/main/java/patrone/david/agent/EchoAgent.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package patrone.david.agent;
+
+import edu.jhuapl.dorset.agents.AbstractAgent;
+import edu.jhuapl.dorset.agents.AgentRequest;
+import edu.jhuapl.dorset.agents.AgentResponse;
+
+/**
+ * Simple agent that just returns your text query.
+ * 
+ * @author davidpatrone
+ *
+ */
+public class EchoAgent extends AbstractAgent {
+
+	@Override
+	public AgentResponse process(AgentRequest request) {
+		AgentResponse r = new AgentResponse(request.getText());
+		return r;
+	}
+}
